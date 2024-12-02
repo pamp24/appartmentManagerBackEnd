e8fd4d94802b2d12be5da54e57a6d446e0dfb20a
pamp24 <pamporakis.fotis@gmail.com>
2024-12-02 21:12:08 +0200

    First-Commit

---
+++ src/main/java/com/example/appartmentManager/AppartmentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.appartmentManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppartmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppartmentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appartmentManager/AppartmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appartmentManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppartmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
