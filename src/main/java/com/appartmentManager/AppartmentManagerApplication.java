f406151c7d53de38c081c64ae8dc7d42efb06f6a
pamp24 <pamporakis.fotis@gmail.com>
2025-03-13 22:13:59 +0200

    ready for first test

--- src/main/java/com/appartmentManager/AppartmentManagerApplication.java
+++ src/main/java/com/appartmentManager/AppartmentManagerApplication.java
@@ -1,8 +1,12 @@
 package com.appartmentManager;
 
+import com.appartmentManager.Roles.Role;
+import com.appartmentManager.Roles.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -16,5 +20,17 @@ public class AppartmentManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AppartmentManagerApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder().name("USER").build()
+				);
+			}
+		};
+	}
+
 
 }
